947616a117b93b0b01644b1c30c4855f98178064
David <davidostos2@gmail.com>
2023-07-17 23:30:38 -0400

    redireccion home login 7

--- src/views/auth/AuthVue.vue
+++ src/views/auth/AuthVue.vue
@@ -1,62 +1,75 @@
 <template>
-    <div class="md:container md:px-6 mx-auto max-w-full lg:max-w-[1140px] font-nunito2">
-      <!-- Outer Row -->
-      <div class="flex justify-center ">
-        <div class="xl:basis-5/6 lg:basis-full md:basis-3/4 basis-full px-4">
-          <div class="cardT overflow-hidden border-0 shadow-lg my-12">
-            <div class="card-body p-0">
-              <!-- Nested Row within Card Body -->
-              <div class="flex justify-center mx-3">
-                <div class="lg:basis-3/6 max-w-[50%] hidden lg:block bg-car-login bg-cover bg-[-9rem] px-3"></div>
-                <div class="lg:basis-3/6 lg:max-w-[50%] w-full px-3">
-                  <div class="md:p-12 p-[1rem_0] ">
-                    <div class="text-center">
-                      <h1 class="text-2xl text-gray-700 mb-6">Welcome Back!</h1>
+  <div class="md:container md:px-6 mx-auto max-w-full lg:max-w-[1140px] font-nunito2">
+    <!-- Outer Row -->
+    <div class="flex justify-center">
+      <div class="xl:basis-5/6 lg:basis-full md:basis-3/4 basis-full px-4">
+        <div class="cardT overflow-hidden border-0 shadow-lg my-12">
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="flex justify-center mx-3">
+              <div
+                class="lg:basis-3/6 max-w-[50%] hidden lg:block bg-car-login bg-cover bg-[-9rem] px-3"
+              ></div>
+              <div class="lg:basis-3/6 lg:max-w-[50%] w-full px-3">
+                <div class="md:p-12 p-[1rem_0]">
+                  <div class="text-center">
+                    <h1 class="text-2xl text-gray-700 mb-6">Welcome Back!</h1>
+                  </div>
+                  <form class="user1">
+                    <div class="form-group mb-4">
+                      <input
+                        type="email"
+                        class="form-control form-control-user"
+                        id="exampleInputEmail"
+                        aria-describedby="emailHelp"
+                        placeholder="Enter Email Address..."
+                        v-model="email"
+                      />
                     </div>
-                    <form class="user1">
-                      <div class="form-group mb-4">
-                        <input
-                          type="email"
-                          class="form-control form-control-user"
-                          id="exampleInputEmail"
-                          aria-describedby="emailHelp"
-                          placeholder="Enter Email Address..."
-                          v-model="email"
-                        />
+                    <div class="form-group mb-4">
+                      <input
+                        type="password"
+                        class="form-control form-control-user"
+                        id="exampleInputPassword"
+                        placeholder="Password"
+                        v-model="password"
+                      />
+                    </div>
+                    <div class="form-group mb-4">
+                      <div class="custom-control custom-checkbox text-sm font-normal">
+                        <input type="checkbox" class="custom-control-input" id="customCheck" />
+                        <label class="custom-control-label" for="customCheck">Remember Me</label>
                       </div>
-                      <div class="form-group mb-4">
-                        <input
-                          type="password"
-                          class="form-control form-control-user"
-                          id="exampleInputPassword"
-                          placeholder="Password"
-                          v-model="password"
-                        />
-                      </div>
-                      <div class="form-group mb-4">
-                        <div class="custom-control custom-checkbox text-sm font-normal">
-                          <input type="checkbox" class="custom-control-input" id="customCheck" />
-                          <label class="custom-control-label" for="customCheck">Remember Me</label>
-                        </div>
-                      </div>
-                      <burron href="index.html" class="btn btn-primary btn-user btn-block"
-                      @click.prevent="loginUser"> Login </burron>
-                      <hr />
-                      <burron href="index.html" class="btn btn-google btn-user btn-block"
-                      @click.prevent="logInGoogle">
-                        <i class="fab fa-google fa-fw"></i> Login with Google
-                      </burron>
-                      <burron href="index.html" class="btn btn-facebook btn-user btn-block">
-                        <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                      </burron>
-                    </form>
+                    </div>
+                    <burron
+                      href="index.html"
+                      class="btn btn-primary btn-user btn-block"
+                      @click.prevent="loginUser"
+                    >
+                      Login
+                    </burron>
                     <hr />
-                    <div class="text-center">
-                      <RouterLink :to="{ name: 'recuperar' }" class="text-sm font-normal">¿Olvidaste la contraseña?</RouterLink>
-                    </div>
-                    <div class="text-center">
-                      <RouterLink :to="{ name: 'registrar' }" class="text-sm font-normal"> ¡Crear cuenta! </RouterLink>
-                    </div>
+                    <burron
+                      href="index.html"
+                      class="btn btn-google btn-user btn-block"
+                      @click.prevent="logInGoogle"
+                    >
+                      <i class="fab fa-google fa-fw"></i> Login with Google
+                    </burron>
+                    <burron href="index.html" class="btn btn-facebook btn-user btn-block">
+                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                    </burron>
+                  </form>
+                  <hr />
+                  <div class="text-center">
+                    <RouterLink :to="{ name: 'recuperar' }" class="text-sm font-normal"
+                      >¿Olvidaste la contraseña?</RouterLink
+                    >
+                  </div>
+                  <div class="text-center">
+                    <RouterLink :to="{ name: 'registrar' }" class="text-sm font-normal">
+                      ¡Crear cuenta!
+                    </RouterLink>
                   </div>
                 </div>
               </div>
@@ -65,6 +78,7 @@
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -106,16 +120,12 @@ onMounted(() => {
 // en la funcion loginGoogle es donde se reciben los datos
 
 async function logInGoogle() {
-  setTimeout( async()=>{
-    try{
-      const response = await GoogleAuth.signIn();
+  try {
+    const response = await GoogleAuth.signIn();
+  } finally {
+    router.push({ name: 'home' });
 
-    }finally{
-      router.push({ name: 'home' }); 
-
-    }
-  },500)
-/*   
+    /*
   console.log(response.authentication.accessToken);
   console.log(response.serverAuthCode);
   console.log(response.email);
@@ -123,10 +133,7 @@ async function logInGoogle() {
   console.log(response.givenName);
   console.log(response.imageUrl);
   */
-
-}
-function retornar () {
-  
+  }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -143,25 +150,25 @@ const authUser = async () => {
 
 <style scoped lang="scss">
 a {
-    color: #4e73df;
-    text-decoration: none;
-    background-color: transparent;
+  color: #4e73df;
+  text-decoration: none;
+  background-color: transparent;
 }
 .cardT {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    min-width: 0;
-    word-wrap: break-word;
-    background-color: #fff;
-    background-clip: border-box;
-    border: 1px solid #e3e6f0;
-    border-radius: 0.35rem;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  min-width: 0;
+  word-wrap: break-word;
+  background-color: #fff;
+  background-clip: border-box;
+  border: 1px solid #e3e6f0;
+  border-radius: 0.35rem;
 }
 .card-body {
-    flex: 1 1 auto;
-    min-height: 1px;
-    padding: 0;
+  flex: 1 1 auto;
+  min-height: 1px;
+  padding: 0;
 }
 form.user1 .custom-checkbox label {
   line-height: 1.5rem;
@@ -177,107 +184,109 @@ form.user1 .btn-user {
   padding: 0.75rem 1rem;
 }
 .form-control {
-    display: block;
-    width: 100%;
-    height: calc(1.5em + 0.75rem + 2px);
-    padding: 0.375rem 0.75rem;
-    font-size: 1rem;
-    font-weight: 400;
-    line-height: 1.5;
-    color: #6e707e;
-    background-color: #fff;
-    background-clip: padding-box;
-    border: 1px solid #d1d3e2;
-    border-radius: 0.35rem;
-    transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  display: block;
+  width: 100%;
+  height: calc(1.5em + 0.75rem + 2px);
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #6e707e;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #d1d3e2;
+  border-radius: 0.35rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
 }
 .custom-control {
-    position: relative;
-    z-index: 1;
-    display: block;
-    min-height: 1.5rem;
-    padding-left: 1.5rem;
+  position: relative;
+  z-index: 1;
+  display: block;
+  min-height: 1.5rem;
+  padding-left: 1.5rem;
 }
 
-#customCheck{
-    box-sizing: border-box;
-    padding: 0;
+#customCheck {
+  box-sizing: border-box;
+  padding: 0;
 }
 .custom-control-input {
-    position: absolute;
-    left: 0;
-    z-index: -1;
-    width: 1rem;
-    height: 1.25rem;
-    opacity: 0;
+  position: absolute;
+  left: 0;
+  z-index: -1;
+  width: 1rem;
+  height: 1.25rem;
+  opacity: 0;
 }
 .custom-control-label {
-    position: relative;
-    margin-bottom: 0;
-    vertical-align: top;
-    color: #858796
+  position: relative;
+  margin-bottom: 0;
+  vertical-align: top;
+  color: #858796;
 }
 .custom-checkbox .custom-control-label::before {
-    border-radius: 0.35rem;
+  border-radius: 0.35rem;
 }
-.custom-control-label::before, .custom-file-label, .custom-select {
-    transition: background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+.custom-control-label::before,
+.custom-file-label,
+.custom-select {
+  transition: background-color 0.15s ease-in-out, border-color 0.15s ease-in-out,
+    box-shadow 0.15s ease-in-out;
 }
 .custom-control-label::before {
-    position: absolute;
-    top: 0.25rem;
-    left: -1.5rem;
-    display: block;
-    width: 1rem;
-    height: 1rem;
-    pointer-events: none;
-    content: "";
-    background-color: #fff;
-    border: #b7b9cc solid 1px;
+  position: absolute;
+  top: 0.25rem;
+  left: -1.5rem;
+  display: block;
+  width: 1rem;
+  height: 1rem;
+  pointer-events: none;
+  content: '';
+  background-color: #fff;
+  border: #b7b9cc solid 1px;
 }
 
 .custom-control-label::after {
-    position: absolute;
-    top: 0.25rem;
-    left: -1.5rem;
-    display: block;
-    width: 1rem;
-    height: 1rem;
-    content: "";
-    background: 50%/50% 50% no-repeat;
+  position: absolute;
+  top: 0.25rem;
+  left: -1.5rem;
+  display: block;
+  width: 1rem;
+  height: 1rem;
+  content: '';
+  background: 50%/50% 50% no-repeat;
 }
 
 .btn:not(:disabled):not(.disabled) {
-    cursor: pointer;
+  cursor: pointer;
 }
 
 .btn-block {
-    display: block;
-    width: 100%;
+  display: block;
+  width: 100%;
 }
 .btn-primary {
-    color: #fff;
-    background-color: #4e73df;
-    border-color: #4e73df;
+  color: #fff;
+  background-color: #4e73df;
+  border-color: #4e73df;
 }
 
 hr {
-    margin-top: 1rem;
-    margin-bottom: 1rem;
-    border: 0;
-    border-top: 1px solid rgba(0, 0, 0, 0.1);
-    box-sizing: content-box;
-    height: 0;
-    overflow: visible;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  border: 0;
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  box-sizing: content-box;
+  height: 0;
+  overflow: visible;
 }
 
 .btn-block + .btn-block {
-    margin-top: 0.5rem;
+  margin-top: 0.5rem;
 }
 .btn-facebook {
-    color: #fff;
-    background-color: #3b5998;
-    border-color: #fff;
+  color: #fff;
+  background-color: #3b5998;
+  border-color: #fff;
 }
-
 </style>
